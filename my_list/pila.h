918bce3df2b8b45cf10c73c27a44b682c5bd530b
Alex23013 <alecornejolupa2013@gmail.com>
2015-11-18 18:16:57 -0500

    Update pila.h

--- my_list/pila.h
+++ my_list/pila.h
@@ -1,10 +1,11 @@
 using namespace std;
+template<typename T>
 class pila
 {
 public:
-	nodo *start;
-	nodo *end;
-	nodo *cent;
+	nodo<T> *start;
+	nodo<T> *end;
+	nodo<T> *cent;
 	
 	
 	int size ;
@@ -17,7 +18,7 @@ public:
 		{return true;}
 		return false;
 		}	
-	void add_nodo(int dato)
+	void add_nodo(T dato)
 	{
 		cent=new nodo(dato);
 		size++;
@@ -54,6 +55,57 @@ public:
 		cent =cent->ant;
 		}
 	cout <<" "<< start->dato<<endl;
+	}
+	pila operator+(const pila o,const pila p) {
+	pila tmp ;
+	nodo<T> *cent;
+	cent=o->end;
+	for(int i =0;i<o.size;i++)
+		{
+		while(cent->ant)
+			{
+			tmp.add_nodo(cent->dato); 
+			cent =cent->ant;
+			}
+		tmp.add_nodo(start->dato);		
+		}
+	cent=p->end;
+	for(int i =0;i<p.size;i++)
+		{
+		while(cent->ant)
+			{
+			tmp.add_nodo(cent->dato); 
+			cent =cent->ant;
+			}
+		tmp.add_nodo(start->dato);		
+		}
+	return tmp;
+	}
+	lista operator-(const pila o,const pila p) {
+	lista tmp ;
+	nodo<T> *cent;
+	cent=o->end;
+	for(int i =0;i<o.size;i++)
+		{
+		while(cent->ant)
+			{
+			tmp.add_nodo(cent->dato); 
+			cent =cent->ant;
+			}
+		tmp.add_nodo(start->dato);	
+		}
+	cent=p->end;
+	for(int i =0;i<p.size;i++)
+		{
+		while(cent)
+			{
+			cent = find(cent->dato);
+			if (cent == NULL)
+				{tmp.add_nodo(cent->dato);} 
+			cent =cent->ant;
+			}
+		}
+	return tmp;	
 	}
 	
 
