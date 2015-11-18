db43f0dc05111206ee9130f4a1543787baffc8a6
Alex23013 <alecornejolupa2013@gmail.com>
2015-11-18 17:12:57 -0500

    Update nodo.h

--- my_list/nodo.h
+++ my_list/nodo.h
@@ -1,13 +1,14 @@
+template<typename T>
 class nodo
 {
 public:
-	int dato;
-	nodo *sig;
-	nodo *ant;
+	T dato;
+	nodo<T> *sig;
+	nodo<T> *ant;
 
-nodo(int dato=0)
+nodo(T dato=0)
 	{this->dato=dato;
 	sig=NULL;ant=NULL;}
 
-void add_dato(int dato){this->dato=dato;}
+void add_dato(t dato){this->dato=dato;}
 };
