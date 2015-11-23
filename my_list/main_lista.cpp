68d346a3eb5ebd6b93f8771994d39c38f3b11480
Alex23013 <alecornejolupa2013@gmail.com>
2015-11-23 15:02:14 -0500

    Update main_lista.cpp

--- my_list/main_lista.cpp
+++ my_list/main_lista.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "nodo.h"
+
 #include "lista.h"
 using namespace std;
 
@@ -8,7 +8,6 @@ void caratula()
 	cout << "Ingrese una opcion: "<<endl;
 	cout << "1: añadir nodo"<<endl;
 	cout << "2: quitar nodo"<<endl;
-	cout << "3:imprimir lista"<<endl;
 	
 }
 int main()
@@ -21,20 +20,45 @@ int main()
 	caratula();
 	cin >> opcion;
 	if(opcion ==1)	
-		{cout << "que dato desea añadir ? "<< endl;
-		int n;
-		cin>>n;		
-		mylista.add_nodo(n);}
+		{
+		int datos;
+		cout << "cuantos datos quiere añadir" <<endl;
+		cin>>datos;	
+		for(int i =0;i< datos;i++)
+			{cout << "Ingrese el dato "<< i+1 <<endl;
+			int n;
+			cin>>n;		
+			mylista.add_nodo(n);
+			}
+		cout << "la nueva lista es " <<endl;
+		mylista.print_lista();
+		}
 	if(opcion ==2)	
 		{cout << "que dato desea quitar ? "<< endl;
 		int n;
 		cin>>n;		
-		mylista.quitar_nodo(n);}
-	if(opcion ==3)	
-		{cout << "la lista es: "<< endl;
-		mylista.print_lista();}
+		mylista.quitar_nodo(n);
+		cout << "la lista ahora es " <<endl;
+		mylista.print_lista();
+		}
+	
 	cout << "Que desea hacer ahora? "<<endl;
 	cout << "1: volver al menu    0: salir"<<endl;
 	cin >> salida;
 	}
+	lista b;
+	cout << "una lista de prueba"<<endl;
+	for(int i =0;i< 4;i++)
+		{b.add_nodo(i);}
+	b.print_lista();
+	
+	lista c;
+	cout <<"La suma: "<<endl;
+	c=mylista+b;
+	c.print_lista();
+	cout <<"La resta: "<<endl;
+	c=mylista-b;
+	c.print_lista();
+
+	
 }
