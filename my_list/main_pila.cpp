c8d010169bf606a5cac13dd24cf1397392e82d14
Alex23013 <alecornejolupa2013@gmail.com>
2015-11-23 15:14:08 -0500

    Update main_pila.cpp

--- my_list/main_pila.cpp
+++ my_list/main_pila.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "nodo.h"
 #include "pila.h"
 using namespace std;
 
@@ -12,7 +11,7 @@ void caratula()
 }
 int main()
 {
-	pila mypila;
+	pila<int> mypila;
 	int opcion;
 	int salida =5;
 	while(salida != 0)
@@ -20,10 +19,14 @@ int main()
 	caratula();
 	cin >> opcion;
 	if(opcion ==1)	
-		{cout << "que dato desea añadir ? "<< endl;
-		int n;
-		cin>>n;		
-		mypila.add_nodo(n);
+		{int datos;
+		cout << "cuantos datos quiere añadir" <<endl;
+		cin>>datos;	
+		for(int i =0;i< datos;i++)
+			{cout << "Ingrese el dato "<< i+1 <<endl;
+			int n;
+			cin>>n;		
+			mypila.add_nodo(n);}
 		cout << "la nueva pila es " <<endl;
 		mypila.print_pila();}
 	if(opcion ==3)	
@@ -35,4 +38,18 @@ int main()
 	cout << "1: volver al menu    0: salir"<<endl;
 	cin >> salida;
 	}
+	pila<int> b;
+	cout << "una pila de prueba"<<endl;
+	for(int i =0;i< 4;i++)
+		{b.add_nodo(i);}
+	b.print_pila();
+	
+	pila<int> c;
+	cout <<"La suma: "<<endl;
+	c=mypila+b;
+	c.print_pila();
+	cout <<"La resta: "<<endl;
+	c=mypila-b;
+	c.print_pila();
+	
 }
